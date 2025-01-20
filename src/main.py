4845d33f8177a953ecec669674fd551a4489eeba
Miguel Mejía <121742455+dothedada@users.noreply.github.com>
2025-01-20 16:58:44 -0500

    Fixes acces to main

--- src/main.py
+++ src/main.py
@@ -1,7 +1,10 @@
-import os
-
-os.environ["XDG_SESSION_TYPE"] = "x11"
 from window import Window
 
-win = Window(600, 800)
-win.wait_for_close()
+
+def main():
+    win = Window(600, 800)
+    win.wait_for_close()
+
+
+if __name__ == "__main__":
+    main()
--- src/window.py
+++ src/window.py
@@ -4,8 +4,8 @@ from tkinter import Tk, BOTH, Canvas
 class Window:
     def __init__(self, width, height):
         self.__root = Tk()
-        self.__root.title("carajo")
-        self.__canvas = Canvas(master=self.__root, width=width, height=height)
+        self.__root.title("Mazy MAAAZEEEEE!!!")
+        self.__canvas = Canvas(master=self.__root, bg="white" width=width, height=height)
         self.__canvas.pack()
         self.__is_active = False
         self.__root.protocol("WM_DELETE_WINDOW", self.close)
@@ -16,8 +16,11 @@ class Window:
 
     def wait_for_close(self):
         self.__is_active = True
-        while self.__is_active:
-            self.redraw()
+        self.__root.mainloop()
+        # while self.__is_active:
+        #     self.redraw()
+        print("window is closed...")
 
     def close(self):
         self.__is_active = False
+        self.__root.destroy()
