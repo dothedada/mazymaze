1d0e7acf2d47db1417f14f20924d08ac9666326c
Miguel Mejía <121742455+dothedada@users.noreply.github.com>
2025-01-25 15:38:12 -0500

    Adds maze to main for visual test

--- src/main.py
+++ src/main.py
@@ -1,33 +1,18 @@
-from graphics import Window, Point
-from cell import Cell
+from graphics import Window
+from maze import Maze
 
 
 def main():
-    win = Window(600, 800)
+    rows = 12
+    cols = 16
+    margin = 50
+    screen_x = 800
+    screen_y = 600
+    cell_size_x = (screen_x - 2 * margin) / cols
+    cell_size_y = (screen_y - 2 * margin) / rows
+    win = Window(screen_x, screen_y)
 
-    c1 = Cell(win)
-    c1.has_right_wall = False
-    c1.draw(Point(50, 50), Point(100, 100))
-
-    c2 = Cell(win)
-    c2.has_left_wall = False
-    c2.has_bottom_wall = False
-    c2.draw(Point(100, 50), Point(150, 100))
-
-    c1.draw_move(c2)
-
-    c3 = Cell(win)
-    c3.has_top_wall = False
-    c3.has_right_wall = False
-    c3.draw(Point(100, 100), Point(150, 150))
-
-    c2.draw_move(c3)
-
-    c4 = Cell(win)
-    c4.has_left_wall = False
-    c4.draw(Point(150, 100), Point(200, 150))
-
-    c3.draw_move(c4, True)
+    maze = Maze(margin, margin, rows, cols, cell_size_x, cell_size_y, win)
 
     win.wait_for_close()
 
