8351fbe72cf0e5d590e19be3a16bcd51f7dd60e3
Miguel Mejía <121742455+dothedada@users.noreply.github.com>
2025-02-13 11:58:50 -0500

    Allows simplify input

--- src/main.py
+++ src/main.py
@@ -3,26 +3,18 @@ from maze import Maze
 
 
 def main():
-    rows = input("Rows: ")
-    cols = input("Columns: ")
+    rows = int(input("Rows: "))
+    cols = int(input("Columns: "))
     margin = 50
     screen_x = 800
     screen_y = 600
     # cell_size_y = 20
     # cell_size_x = 20
-    cell_size_x = (screen_x - 2 * margin) / int(cols)
-    cell_size_y = (screen_y - 2 * margin) / int(rows)
+    cell_size_x = (screen_x - 2 * margin) / cols
+    cell_size_y = (screen_y - 2 * margin) / rows
     win = Window(screen_x, screen_y)
 
-    maze = Maze(
-        margin,
-        margin,
-        int(rows),
-        int(cols),
-        cell_size_x,
-        cell_size_y,
-        win,
-    )
+    maze = Maze(margin, margin, rows, cols, cell_size_x, cell_size_y, win)
     maze.solve()
 
     win.wait_for_close()
