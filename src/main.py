20aa553d113f392a249bbc7a2743148c5e241f10
Miguel Mejía <121742455+dothedada@users.noreply.github.com>
2025-02-13 11:51:24 -0500

    Allows user input

--- src/main.py
+++ src/main.py
@@ -3,18 +3,26 @@ from maze import Maze
 
 
 def main():
-    rows = 5
-    cols = 5
+    rows = input("Rows: ")
+    cols = input("Columns: ")
     margin = 50
     screen_x = 800
     screen_y = 600
-    cell_size_x = 20
-    cell_size_y = 20
-    # cell_size_x = (screen_x - 2 * margin) / cols
-    # cell_size_y = (screen_y - 2 * margin) / rows
+    # cell_size_y = 20
+    # cell_size_x = 20
+    cell_size_x = (screen_x - 2 * margin) / int(cols)
+    cell_size_y = (screen_y - 2 * margin) / int(rows)
     win = Window(screen_x, screen_y)
 
-    maze = Maze(margin, margin, rows, cols, cell_size_x, cell_size_y, win)
+    maze = Maze(
+        margin,
+        margin,
+        int(rows),
+        int(cols),
+        cell_size_x,
+        cell_size_y,
+        win,
+    )
     maze.solve()
 
     win.wait_for_close()
